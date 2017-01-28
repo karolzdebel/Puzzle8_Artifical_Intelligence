35cfdcfa9c19662d100bf335c720e3f530eeceaf
karolzdebel <kzdebel@mail.uoguelph.ca>
2017-01-28 17:49:31 -0500

    Implemented board class, puzzleProblem, and puzzleAgent are in the process of being developed

--- src/intelligent_systems_a1/PuzzleAgent.java
+++ src/intelligent_systems_a1/PuzzleAgent.java
@@ -1,5 +1,11 @@
 package intelligent_systems_a1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 /*PuzzleAgent.java: This class launches agent application, and should be a subclass of SearchAgent described
 above. It loads initial state and goal state from a file. It must implement showSolution(),showTree(),
 and insertFringe() as required by SearchAgent.*/
@@ -9,6 +15,30 @@ and insertFringe() as required by SearchAgent.*/
  *
  * @author K
  */
-public class PuzzleAgent {
-    
+public final class PuzzleAgent extends SearchAgent {
+
+    @Override
+    void showSolution() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    void showTree() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    void insertFringe(Node nd, LinkedList<Node> ll) {
+        
+        Board insertBoard = (Board)nd.getState();
+        
+        for (int i=0;i<ll.size();i++){
+            Board curBoard = (Board)ll.get(i).getState();
+
+            //find lowest heuristic value position
+            if (insertBoard.getHeurValue() < curBoard.getHeurValue()){
+                ll.add(i,nd);
+            }
+        }
+    }
 }
